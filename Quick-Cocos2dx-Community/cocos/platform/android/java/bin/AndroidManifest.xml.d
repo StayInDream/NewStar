e54d3ad98e41eee3422d245e2ac13e2702e35e6a
不爱吃肉 <384427710@qq.com>
2016-12-09 09:42:37 +0800

    player

---
+++ Quick-Cocos2dx-Community/cocos/platform/android/java/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/u0u0/Documents/TT/Quick-Cocos2dx-Community/cocos/platform/android/java/bin/AndroidManifest.xml : \
+/Users/u0u0/Documents/TT/Quick-Cocos2dx-Community/cocos/platform/android/java/AndroidManifest.xml \
---
+++ Quick-Cocos2dx-Community/cocos/platform/android/java/gen/R.java.d
@@ -0,0 +1,1 @@
+ : /Users/u0u0/Documents/TT/Quick-Cocos2dx-Community/cocos/platform/android/java/bin/AndroidManifest.xml \
